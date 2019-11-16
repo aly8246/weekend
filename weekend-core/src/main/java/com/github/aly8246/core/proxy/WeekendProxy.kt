a23613f3834a7e37ea2f4a54a0127444c5d36607
如风 <1558146696@qq.com>
2019-11-17 02:56:08 +0800

    新增执行时间计算

--- weekend-core/src/main/java/com/github/aly8246/core/proxy/WeekendProxy.kt
+++ weekend-core/src/main/java/com/github/aly8246/core/proxy/WeekendProxy.kt
@@ -2,10 +2,11 @@ package com.github.aly8246.core.proxy
 
 
 import com.github.aly8246.core.dispatcher.DispatcherFactory
-import com.github.aly8246.core.dispatcher.DispatcherFactoryPolicy
-
+import com.github.aly8246.core.util.PrintImpl
 import java.lang.invoke.MethodHandles
-import java.lang.reflect.*
+import java.lang.reflect.InvocationHandler
+import java.lang.reflect.Method
+import java.lang.reflect.Modifier
 
 
 class WeekendProxy<T>(private val target: Class<T>) : InvocationHandler {
@@ -15,9 +16,17 @@ class WeekendProxy<T>(private val target: Class<T>) : InvocationHandler {
             Any::class.java == method.declaringClass -> method.invoke(this, args)//class类直接执行
             isDefaultMethod(method) -> invokeDefaultMethod(proxy, method, args)//默认方法直接执行
             else -> {
+                val startTime = System.currentTimeMillis()
+                PrintImpl().info("weekend startup!")
                 val dispatcherFactoryPolicy = DispatcherFactory<T>()
                 val dispatcherFactory = dispatcherFactoryPolicy.dispatcherFactory(proxy, method, args)
-                dispatcherFactory.execute()
+
+                val result = dispatcherFactory.execute()
+                PrintImpl().info("weekend query success!")
+                val endTime = System.currentTimeMillis()
+                val seconds = (endTime - startTime) / 1000f
+                PrintImpl().info("spend time: $seconds seconds.")
+                return result
             }
         }
 
@@ -31,6 +40,7 @@ class WeekendProxy<T>(private val target: Class<T>) : InvocationHandler {
         when {
             !constructor.isAccessible -> constructor.isAccessible = true
         }
+
         val declaringClass = method.declaringClass
         return constructor.newInstance(declaringClass,
                 MethodHandles.Lookup.PRIVATE or MethodHandles.Lookup.PROTECTED
