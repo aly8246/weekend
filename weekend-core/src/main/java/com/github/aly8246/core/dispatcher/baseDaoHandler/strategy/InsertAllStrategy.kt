25d2d7108d8bc8c8dc56f59a471832d36cd27f9e
南有乔木 <1558146696@qq.com>
2019-11-25 12:13:20 +0800

    补上批量插入策略的注释

--- weekend-core/src/main/java/com/github/aly8246/core/dispatcher/baseDaoHandler/strategy/InsertAllStrategy.kt
+++ weekend-core/src/main/java/com/github/aly8246/core/dispatcher/baseDaoHandler/strategy/InsertAllStrategy.kt
@@ -10,7 +10,10 @@ class InsertAllStrategy<T> : CollectionEntityResolver(), BaseDaoStrategy<T> {
     override fun createBaseCommand(proxy: Any, method: Method, args: Array<Any>?, mongoConnection: MongoConnection, target: Class<T>): String {
         val collectionName = this.collectionName(target)
 
+        //实体集合
         val entityList = args!![0] as MutableList<*>
+
+        //实体不为空的字段和值
         val resolverEntity: MutableList<MutableMap<String, Any>> = mutableListOf()
         for (entity in entityList) {
             if (entity != null) {
@@ -18,12 +21,14 @@ class InsertAllStrategy<T> : CollectionEntityResolver(), BaseDaoStrategy<T> {
             }
         }
 
+        //值 (1,2,3),(4,5,6)
         val entityValueList: MutableList<String> = mutableListOf()
         resolverEntity.forEach { e ->
             run {
                 entityValueList.add("(${e.values.joinToString(",")})")
             }
         }
+
         return "INSERT INTO $collectionName (${resolverEntity[0].keys.joinToString(",")}) values ${entityValueList.joinToString(",")}"
     }
 }
